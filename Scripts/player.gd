90e0cb02cdd48d7be8c132e03d793832b52dd5e0
FlakeyMcJakey <jacob.kuenning@gmail.com>
2025-03-29 09:39:37 -0700

    Push 4
    Player now movable

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,13 +1,34 @@
 extends CharacterBody3D
-const SPEED = 5.0
+@export var gm : Node3D
+const SPEED = 8
+const ACCEL = 2
+const FRIC = 1
+var look_dir: Vector2
 
+@export var mouse_sens = 50
 
 func _physics_process(delta):
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		accelerate(direction)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		apply_friction()	
+	
+	_rotate_camera(delta)
+	move_and_slide()	
+
+func accelerate(direction):
+	velocity = velocity.move_toward(direction * SPEED, ACCEL)
+
+func apply_friction():
+	velocity = velocity.move_toward(Vector3.ZERO,FRIC)
+
+func _input(event: InputEvent): #handle mouse motion for looking left and right
+		if event is InputEventMouseMotion: look_dir = event.relative * 0.01 
+	
+func _rotate_camera(delta: float):
+		var input = Input.get_vector("look_left", "look_right", "NULL", "NULL") #NULL because we only need the first two fields
+		look_dir += input
+		rotation.y -= look_dir.x * mouse_sens * delta
+		look_dir = Vector2.ZERO
