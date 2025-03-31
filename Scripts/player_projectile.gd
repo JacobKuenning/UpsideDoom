6823d141abd1c96ed4099c2eef06972bc254903f
FlakeyMcJakey <jacob.kuenning@gmail.com>
2025-03-30 23:47:10 -0700

    push

--- Scripts/player_projectile.gd
+++ Scripts/player_projectile.gd
@@ -1,7 +1,9 @@
 extends RigidBody3D
 @export var speed = 40
+@export var timer : Timer
 
 func _ready():
+	timer.start()
 	pass
 
 func _physics_process(delta):
@@ -14,5 +16,10 @@ func _on_area_3d_body_entered(body: Node3D) -> void:
 
 
 func _on_area_3d_area_entered(area: Area3D) -> void:
+	queue_free()
+	pass # Replace with function body.
+
+
+func _on_timer_timeout() -> void:
 	queue_free()
 	pass # Replace with function body.
